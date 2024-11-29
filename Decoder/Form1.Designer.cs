ca2236b1bd8eae587cd836f7a3f8e0a26a628250
SeungHwan <a77285952@gmail.com>
2024-11-29 19:51:57 +0900

    decorder
    c# 디코더

---
+++ Decoder/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Decoder
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.GoupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.GoupBox1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(39, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "암호문";
+            // 
+            // GoupBox1
+            // 
+            this.GoupBox1.Controls.Add(this.tbInput);
+            this.GoupBox1.Controls.Add(this.label2);
+            this.GoupBox1.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.GoupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.GoupBox1.Location = new System.Drawing.Point(37, 36);
+            this.GoupBox1.Name = "GoupBox1";
+            this.GoupBox1.Size = new System.Drawing.Size(545, 84);
+            this.GoupBox1.TabIndex = 2;
+            this.GoupBox1.TabStop = false;
+            this.GoupBox1.Text = "입력";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(148, 34);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(349, 41);
+            this.tbInput.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(37, 150);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(545, 104);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "출력값";
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtResult.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtResult.ForeColor = System.Drawing.Color.Green;
+            this.txtResult.Location = new System.Drawing.Point(39, 37);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(458, 49);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Text = "암호문";
+            this.txtResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(37, 305);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(151, 43);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "확인";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(235, 305);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(151, 43);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "초기화";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(431, 305);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(151, 43);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "파일열기";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 386);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.GoupBox1);
+            this.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "FrmMain";
+            this.Text = "Decoder";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.GoupBox1.ResumeLayout(false);
+            this.GoupBox1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox GoupBox1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Decoder/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Decoder
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = Process(tbInput.Text);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "암호문";
+            tbInput.Text = "";
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadFile();
+        }
+
+        private string Process(string sValue)
+        {
+            string sResult = "";
+
+            for (int i = 0; i < sValue.Length; i++)
+            {
+                if (sValue[i] == ' ') sResult += " ";
+                else if (sValue[i] == 'a') sResult += "x";
+                else if (sValue[i] == 'b') sResult += "y";
+                else if (sValue[i] == 'c') sResult += "z";
+                else
+                {
+                    char value = (char)(sValue[i] - 3);
+                    sResult += value.ToString();
+                }
+            }
+
+            SaveFile();
+
+            return sResult;
+        }
+
+        private void SaveFile()
+        {
+            string sValue = tbInput.Text;
+
+            StreamWriter sw = new StreamWriter("data.txt");
+            sw.WriteLine(sValue);
+
+            sw.Close();
+        }
+
+        private void LoadFile()
+        {
+            if (!File.Exists("data.txt"))
+                return;
+
+            try
+            {
+                StreamReader sr = new StreamReader("data.txt");
+                string sValue = sr.ReadLine();
+
+                sr.Close();
+
+                tbInput.Text = sValue;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
